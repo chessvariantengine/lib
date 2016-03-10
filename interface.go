48f4f83484fb9f4bddfeb7904c4defd57451e0e7
scalachessgui <scalachessgui@gmail.com>
2016-03-10 11:14:49 +0100

    Command loop

--- interface.go
+++ interface.go
@@ -10,6 +10,9 @@ package lib
 import(
 	"fmt"
 	"os"
+	"bufio"
+	"log"
+	"strings"
 )
 
 //////////////////////////////////////////////////////
@@ -20,38 +23,229 @@ import(
 // this allows to have an standalone executable for every variant/protocol combination
 
 func Run(variant int, protocol int) {
-	Printu(fmt.Sprintf("variantengine %s %s by golang\n",VARIANT_TO_NAME[variant],PROTOCOL_TO_NAME[protocol]))
+	// set current variant
+	Variant = variant
+	// set current protocol
+	Protocol = protocol
+	// print introduction
+	Printu(Intro())
+
+	// set up logging
+	log.SetOutput(os.Stdout)
+	log.SetPrefix("info string ")
+	log.SetFlags(log.Lshortfile)
+
+	// command interpreter main loop
+	scan := bufio.NewScanner(os.Stdin)
+
+	for scan.Scan() {
+		scannedline := scan.Text()
+		err := ExecuteLine(scannedline)
+		if err == errQuit {
+			break
+		}
+	}
+
+	if scan.Err() != nil {
+		log.Println(scan.Err())
+	}
 }
 
 //////////////////////////////////////////////////////
 
-// enumeration of variants
+// line read from stdin for execution
+var line string
 
+// line args
+var args []string
+
+// number of args
+var numargs int
+
+// argument pointer
+var argptr int
+
+// line command
+var command string
+
+///////////////////////////////////////////////
+// ExecuteLine : execute command line
+// <- error : error
+
+func ExecuteLine(setline string) error {
+	line = strings.TrimSpace(setline)
+	args = strings.Fields(line)
+	var err error = nil
+	if len(args)>0 {
+		// only try to execute a line that has at least one token
+		// first token is the command
+		command = args[0]
+		// rest are the arguments
+		args = args[1:]	
+		numargs = len(args)
+		argptr = 0
+		// first look at test commands
+		if TEST {
+			err = ExecuteTest()
+		}
+		// if test did not handle the command execute it by protocol
+		if err == nil { switch Protocol {
+			case PROTOCOL_UCI: err = ExecuteUci()
+			case PROTOCOL_XBOARD : err = ExecuteXboard()
+		}}
+		if err != nil {
+			if err != errQuit && err != errTestOk {
+				log.Println(err)
+			}
+		}
+	}
+	return err
+}
+
+///////////////////////////////////////////////
+// ExecuteTest : execute TEST command
+// <- error : error
+
+func ExecuteTest() error {
+	switch command {
+		case "x": return errQuit
+		case "intro":
+			fmt.Print(Intro())
+			return errTestOk
+		case "sv":
+			if numargs>0 {
+				ok := false
+				setvariant := GetRest()
+				variant,found := VARIANT_NAME_TO_VARIANT[setvariant]
+				if found {
+					Variant = variant
+					ok = true
+				}
+				variant,found = VARIANT_SHORTHAND_NAME_TO_VARIANT[setvariant]
+				if found {
+					Variant = variant
+					ok = true
+				}
+				if ok {
+					fmt.Printf("variant set to %s\n",VARIANT_TO_NAME[Variant])
+					return nil
+				} else {
+					fmt.Printf("unknown variant %s\n",args[0])
+					return nil
+				}
+			} else {
+				fmt.Printf("current variant %s\n",VARIANT_TO_NAME[Variant])
+				return nil
+			}
+	}
+	return nil
+}
+
+///////////////////////////////////////////////
+
+///////////////////////////////////////////////
+// ExecuteUci : execute UCI command
+// <- error : error
+
+func ExecuteUci() error {
+	switch command {
+	case "uci":
+		Printu(fmt.Sprintf("id name %s\n",VARIANT_TO_ENGINE_NAME[Variant]))
+		Printu(fmt.Sprintf("id author %s\n",Author))
+		Printu("\n")
+		Printu("option name UCI_AnalyseMode type check default false\n")
+		Printu("option name Ponder type check default true\n")
+		Printu("uciok\n")
+		return nil
+	case "quit": return errQuit
+	}
+	return nil
+}
+
+///////////////////////////////////////////////
+
+///////////////////////////////////////////////
+// ExecuteUci : execute XBOARD command
+// <- error : error
+
+func ExecuteXboard() error {
+	return nil
+}
+
+///////////////////////////////////////////////
+
+// Author
+var Author = "Alexandru Mosoi"
+
+// current variant
+var Variant int
+
+// current protocol
+var Protocol int
+
+// test mode
+var TEST bool = true
+
+// enumeration of variants
 const(
 	VARIANT_Standard          = iota             
 	VARIANT_Racing_Kings
 )
 
 // enumeration of protocols
-
 const(
 	PROTOCOL_UCI              = iota
 	PROTOCOL_XBOARD
 )
 
 // names of variants
-
 var VARIANT_TO_NAME=[...]string{
 	"Standard",
 	"Racing Kings",
 }
 
-// names of protocols
+// variant name to variant
+var VARIANT_NAME_TO_VARIANT=map[string]int{
+	"Standard": VARIANT_Standard,
+	"Racing Kings": VARIANT_Racing_Kings,
+}
 
+var VARIANT_SHORTHAND_NAME_TO_VARIANT=map[string]int{
+	"s": VARIANT_Standard,
+	"rk": VARIANT_Racing_Kings,
+}
+
+// variant to engine name
+var VARIANT_TO_ENGINE_NAME=[...]string{
+	"zurichess",
+	"verkuci",
+}
+
+// names of protocols
 var PROTOCOL_TO_NAME=[...]string{
 	"UCI",
 	"XBOARD",
 }
+
+// quit application 'error'
+var (
+	errQuit = fmt.Errorf("quit")
+)
+
+// test command ok 'error'
+var (
+	errTestOk = fmt.Errorf("testok")
+)
+
+///////////////////////////////////////////////
+// Intro : introduction
+// <- string : introduction
+
+func Intro() string {
+	return fmt.Sprintf("%s %s chess variant %s engine by %s\n",VARIANT_TO_ENGINE_NAME[Variant],VARIANT_TO_NAME[Variant],PROTOCOL_TO_NAME[Protocol],Author)
+}
+
+///////////////////////////////////////////////
 
 ///////////////////////////////////////////////
 // Printu : unbuffered write to stdout
@@ -59,6 +253,20 @@ var PROTOCOL_TO_NAME=[...]string{
 
 func Printu(str string) {
 	os.Stdout.Write([]byte(str))
+}
+
+///////////////////////////////////////////////
+
+///////////////////////////////////////////////
+// GetRest : get rest of arguments as a single string
+// -> str string : rest of arguments joined by space
+
+func GetRest() string {
+	if (numargs-argptr)>0 {
+		return strings.Join(args[argptr:]," ")
+	} else {
+		return ""
+	}
 }
 
 ///////////////////////////////////////////////
